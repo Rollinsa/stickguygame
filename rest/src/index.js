6163c1abbae8348cd90acc6ffe4dff055e09ec8f
Rollinsa <alexrollins95@gmail.com>
2019-03-11 20:14:54 -0500

    Fixes bugs with mongo connection

--- rest/src/index.js
+++ rest/src/index.js
@@ -2,14 +2,17 @@ const express = require("express");
 const MongoClient = require('mongodb').MongoClient;
 const routerHome = require("./routes/router-home.js");
 const bodyParser = require('body-parser');
-
 const app = express();
 const mongoURL = 'mongodb://mongo:27017';
 const dbName = 'atmDB';
 const collectionName = 'atm';
 
-MongoClient.connect(mongoURL, function(err, client) {
-	console.log("Connected successfully to server");
+MongoClient.connect(mongoURL, { auth: { user: "root", password: "pass" } }, function(err, client) {
+	if (err) {
+		console.error("Error....", err);
+		return;
+	}
+	console.log("Connected successfully to Mongo Client");
 
 	app.use(bodyParser.urlencoded({ extended: false }));
 	app.use(bodyParser.json());
@@ -29,6 +32,4 @@ MongoClient.connect(mongoURL, function(err, client) {
 	app.listen(3000, () => {
 		console.log("Express server up and running!");
 	})
-
-	client.close();
 });
